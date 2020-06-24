6ced0c21cd8b7314544f41d01f2f59da73fc981f
Ahmed Hasan <ahmedmaher05@gmail.com>
2020-06-25 01:27:04 +0200

    adding simple change to dev

--- .history/src/server_20200624171510.ts
+++ .history/src/server_20200624171510.ts
@@ -56,7 +56,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
-// simple change
+
   // Start the Server
   app.listen( port, () => {
       console.log( `server running http://localhost:${ port }` );
